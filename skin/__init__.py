f52dea1bba5ff9145dbeaeb05eafc5eca7041d4b
lishali <lisha@math.berkeley.edu>
2019-09-17 20:17:36 -0400

    error fixed in defining inputIMage

--- skin/__init__.py
+++ skin/__init__.py
@@ -13,6 +13,7 @@ if __name__ == "__main__":
 	args=parser.parse_args()
 
 	#with open(args.img,'rb') as inputImage:
+	args.img = inputImage
 	if(args.which=="color"):
 		a=args.col.strip('[] ')
 		b=a.split(',')

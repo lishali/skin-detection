1c4b0b2f7474a5a7134fe9bc00ea2901755ec497
lishali <lisha@math.berkeley.edu>
2019-09-17 20:15:33 -0400

    testing inserting string for filename instead

--- skin/__init__.py
+++ skin/__init__.py
@@ -12,19 +12,20 @@ if __name__ == "__main__":
 	parser.add_argument("res", help="Path where image is to be saved")
 	args=parser.parse_args()
 
-	with open(args.img,'rb') as inputImage:
-		if(args.which=="color"):
-			a=args.col.strip('[] ')
-			b=a.split(',')
-			result=change_skin(inputImage,[int(b[0].strip()),int(b[1].strip()),int(b[2].strip())], args.res)
-		elif(args.which=="image"):
-			color=obtain_skin_color(args.col)
-			color=np.uint8([[color]])
-			color=cv2.cvtColor(color,cv2.COLOR_HSV2RGB)
-			color=color[0][0]
-			result=change_skin(inputImage,color,args.res)
-		else:
-			print("Please enter correct detection type.")
+	#with open(args.img,'rb') as inputImage:
+	if(args.which=="color"):
+		a=args.col.strip('[] ')
+		b=a.split(',')
+		result=change_skin(inputImage,[int(b[0].strip()),int(b[1].strip()),int(b[2].strip())], args.res)
+	elif(args.which=="image"):
+		color=obtain_skin_color(args.col)
+		color=np.uint8([[color]])
+		color=cv2.cvtColor(color,cv2.COLOR_HSV2RGB)
+		color=color[0][0]
+		result=change_skin(inputImage,color,args.res)
+	else:
+		print("Please enter correct detection type.")
 
 	with open(args.res,'wb') as resultFile:
 		resultFile.write(result)
+
